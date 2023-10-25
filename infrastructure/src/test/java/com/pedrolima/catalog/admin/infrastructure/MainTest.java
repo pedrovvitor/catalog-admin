fc706414af9c15cf7ba0f00bce413ce66c69c9c2
Pedro Lima <pedalm@on.sinch.com>
2023-10-25 14:48:48 -0300

    Test datasource configuration

--- infrastructure/src/test/java/com/pedrolima/catalog/admin/infrastructure/MainTest.java
+++ infrastructure/src/test/java/com/pedrolima/catalog/admin/infrastructure/MainTest.java
@@ -2,11 +2,13 @@ package com.pedrolima.catalog.admin.infrastructure;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
 
 public class MainTest {
 
     @Test
     public void testMain() {
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "test");
         Assertions.assertNotNull(new Main());
         Main.main(new String[]{});
     }
