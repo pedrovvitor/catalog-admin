1d50b783a14f86dc6f4fd411dfa62b7e188a1a48
Pedro Lima <pedalm@on.sinch.com>
2023-10-26 17:31:36 -0300

    Create Category JPA repository

--- infrastructure/src/main/java/com/pedrolima/catalog/admin/infrastructure/Main.java
+++ infrastructure/src/main/java/com/pedrolima/catalog/admin/infrastructure/Main.java
@@ -1,9 +1,16 @@
 package com.pedrolima.catalog.admin.infrastructure;
 
+import com.pedrolima.catalog.admin.domain.category.Category;
+import com.pedrolima.catalog.admin.infrastructure.category.persistence.CategoryJpaEntity;
+import com.pedrolima.catalog.admin.infrastructure.category.persistence.CategoryRepository;
 import com.pedrolima.catalog.admin.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
