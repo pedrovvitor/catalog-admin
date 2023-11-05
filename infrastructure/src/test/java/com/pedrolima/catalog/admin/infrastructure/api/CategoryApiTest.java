92e0ee3fc2ba3d6f4e0f548f887eae251a3b722e
Pedro Lima <pedrojppb@gmail.com>
2023-11-05 11:55:42 -0300

    Create Category Endpoint Integrated Test

--- infrastructure/src/test/java/com/pedrolima/catalog/admin/infrastructure/api/CategoryApiTest.java
+++ infrastructure/src/test/java/com/pedrolima/catalog/admin/infrastructure/api/CategoryApiTest.java
@@ -1,11 +1,27 @@
 package com.pedrolima.catalog.admin.infrastructure.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pedrolima.catalog.admin.ControllerTest;
+import com.pedrolima.catalog.admin.application.category.create.CreateCategoryOutput;
 import com.pedrolima.catalog.admin.application.category.create.CreateCategoryUseCase;
+import com.pedrolima.catalog.admin.domain.category.CategoryID;
+import com.pedrolima.catalog.admin.infrastructure.category.models.CreateCategoryApiInput;
+import io.vavr.API;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Objects;
+
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ControllerTest(controllers = CategoryAPI.class)
 public class CategoryApiTest {
@@ -16,8 +32,36 @@ public class CategoryApiTest {
     @MockBean
     private CreateCategoryUseCase createCategoryUseCase;
 
-    @Test
-    void test() {
+    @Autowired
+    private ObjectMapper mapper;
 
+    @Test
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception {
+        final var expectedName = "Movies";
+        final var expectedDescription = "The most watched category";
+        final var expectedIsActive = true;
+
+        final var aInput = new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        Mockito.when(createCategoryUseCase.execute(Mockito.any()))
+                .thenReturn(API.Right(CreateCategoryOutput.from(CategoryID.from("123"))));
+
+        final var request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(aInput));
+
+        mvc.perform(request)
+                .andDo(print())
+                .andExpectAll(
+                        status().isCreated(),
+                        header().string("Location", "/categories/123")
+                );
+
+        Mockito.verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+
+        ));
     }
 }
