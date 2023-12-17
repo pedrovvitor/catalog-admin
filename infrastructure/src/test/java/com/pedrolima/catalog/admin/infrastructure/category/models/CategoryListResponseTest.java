c7730f7ebcdf3476d4a6af8361d3a4e5f2f756d6
Pedro Lima <pedrojppb@gmail.com>
2023-12-17 18:49:25 -0300

    CategoryListResponse serialization test

---
+++ infrastructure/src/test/java/com/pedrolima/catalog/admin/infrastructure/category/models/CategoryListResponseTest.java
@@ -0,0 +1,44 @@
+package com.pedrolima.catalog.admin.infrastructure.category.models;
+
+import com.pedrolima.catalog.admin.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.time.Instant;
+
+@JacksonTest
+public class CategoryListResponseTest {
+
+    @Autowired
+    private JacksonTester<CategoryListResponse> json;
+
+    @Test
+    public void testMarshall() throws Exception {
+        final var expectedId = "123";
+        final var expectedName = "Filmes";
+        final var expectedDescription = "The most watched category";
+        final var expectedIsActive = true;
+        final var expectedCreatedAt = Instant.now();
+        final var expectedDeletedAt = Instant.now();
+
+        final var response = new CategoryListResponse(
+                expectedId,
+                expectedName,
+                expectedDescription,
+                expectedIsActive,
+                expectedCreatedAt,
+                expectedDeletedAt);
+
+        final var actualJson = json.write(response);
+
+        Assertions.assertThat(actualJson)
+                .hasJsonPathValue("$.id", expectedId)
+                .hasJsonPathValue("$.name", expectedName)
+                .hasJsonPathValue("$.description", expectedDescription)
+                .hasJsonPathValue("$.is_active", expectedIsActive)
+                .hasJsonPathValue("$.created_at", expectedCreatedAt.toString())
+                .hasJsonPathValue("$.deleted_at", expectedDeletedAt.toString());
+    }
+}
