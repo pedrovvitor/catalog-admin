02b0dde40a2f0f7c6fb826c5229d622d79504f21
Pedro Lima <pedrojppb@gmail.com>
2023-12-17 18:45:38 -0300

    UpdateCategoryRequest serialization test

---
+++ infrastructure/src/test/java/com/pedrolima/catalog/admin/infrastructure/category/models/UpdateCategoryRequestTest.java
@@ -0,0 +1,42 @@
+package com.pedrolima.catalog.admin.infrastructure.category.models;
+
+import com.pedrolima.catalog.admin.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+@JacksonTest
+public class UpdateCategoryRequestTest {
+
+    @Autowired
+    private JacksonTester<UpdateCategoryRequest> json;
+
+    @Test
+    public void testUnmarshall() throws IOException {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "The most watched category";
+        final var expectedIsActive = true;
+
+        final var json = """
+                {
+                    "name" : "%s",
+                    "description" : "%s",
+                    "is_active" : "%s"
+                }
+                """.formatted(
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("description", expectedDescription)
+                .hasFieldOrPropertyWithValue("active", expectedIsActive);
+    }
+}
